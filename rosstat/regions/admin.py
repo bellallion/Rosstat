044a432ab467594e609a7fe10c7529e2a45f3a38
AlenaK <alenkakononova2005@gmail.com>
2026-03-07 11:44:06 +0300

    create models RegionsRF and LaborForceRegion

--- rosstat/regions/admin.py
+++ rosstat/regions/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+#---Трудоустройство выпускников по специальностям. Средние профессиональное образование
+class LaborForceRegionInline(admin.TabularInline):
+    model = LaborForceRegion
+    extra = 1
+    fields = ['region', 'year', 'value']
+    ordering = ['year']
+
+
+@admin.register(LaborForceRegion)
+class LaborForceRegionAdmin(admin.ModelAdmin):
+    list_display = ['region', 'year', 'value']
+    list_filter = ['year', 'region']
+    search_fields = ['region__name']
+    # list_editable = ['value']
+
+    
+#---Справочник специальностей
+@admin.register(RegionsRF)
+class RegionsRFAdmin(admin.ModelAdmin):
+    list_display = ['name', 'code']
+    search_fields = ['name', 'code']
+    inlines = [LaborForceRegionInline]
--- rosstat/regions/models.py
+++ rosstat/regions/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class RegionsRF(models.Model):
+    """Справочник регионов России"""
+    name = models.CharField(max_length=255, unique=True, verbose_name="Название региона")
+    code = models.SmallIntegerField(verbose_name="Код региона")
+    
+    class Meta:
+        verbose_name = "Регион"
+        verbose_name_plural = "Регионы"
+        ordering = ['name']
+    
+    def __str__(self):
+        return self.name
+
+class LaborForceRegion(models.Model):
+    """Модель данных о численности рабочей силы по регионам РФ  """
+    region = models.ForeignKey(
+        RegionsRF,
+        on_delete=models.CASCADE,
+        related_name='labor_force',
+        verbose_name="Регион"
+    )
+    year = models.SmallIntegerField(verbose_name="год")
+    value = models.FloatField(verbose_name="Численность рабочей силы")
+
+    class Meta:
+        verbose_name = "Численность рабочей силы по субъектам Российской Федерации"
+        verbose_name_plural = "Численность рабочей силы по субъектам Российской Федерации"
+
+    def __str__(self):
+
+        return f"{self.regionrf.name}-{self.year} : {self.value}"
